943372f0b7406adc2e995d962971d6bc42e26a58
theanarchist123 <2023.nikhil.kadam@ves.ac.in>
2025-06-06 00:13:40 +0530

    Document listing feature implemented...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,37 +4,77 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { getDocuments } from "@/lib/actions/room.actions";
+import Link from "next/link";
+import { dateConverter } from "@/lib/utils";
+import Notifications from "@/components/Notifications";
+import { DeleteModal } from "@/components/DeleteModal";
 const Home = async () => {
   const clerkUser = await currentUser();
-  if(!clerkUser) {
+  if (!clerkUser) {
     redirect("/sign-in");
   }
-  const documents = [];
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
+
   return (
     <main className="home-container">
-<Header className="sticky left-0 top-0">
-  <div className="flex items-center gap-2 lg:gap-4">
-    <p>Notification</p>
-    <SignedIn>
-      <UserButton />
-    </SignedIn>
-  </div>
-</Header>
-{documents.length > 0 ? (
-<div>
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <Notifications />
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
 
-</div>
-):(
-<div className="document-list-empty">
-<Image src="/assets/icons/doc.svg" alt="Document" width={40} height={40} className="mw-auto" / >
-<AddDocumentBtn 
-userId={clerkUser.id}
-email={clerkUser.emailAddresses[0].emailAddress}/>
+      {roomDocuments.data.length > 0 ? (
+        <div className="document-list-container">
+          <div className="document-list-title">
+            <h3 className="text-20-semibold">All Documents</h3>
+            <AddDocumentBtn
+             userId={clerkUser.id}
+             email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+          <ul className="document-ul">
+            {roomDocuments.data.map(({id,metadata,createdAt}:any) => (
+                <li key={id} className="document-list-item">
+                  <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
+                  <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
+                    <Image 
+                    src="/assets/icons/doc.svg" 
+                    alt="file" 
+                    width={40} 
+                    height={40} 
+                     />
+                  </div>
+                  <div className="space-y-1">
+                    <p className="line-clamp-1 text-lg">{metadata.title}</p>
+                    <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
+                  </div>
+                  </Link>
+                  <DeleteModal roomId={id} />
+                </li>
+           ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="document-list-empty">
+          <Image 
+          src="/assets/icons/doc.svg" 
+          alt="Document" 
+          width={40} 
+          height={40} 
+          className="mw-auto" 
+          />
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress} />
 
-</div>
+        </div>
 
 
-)}
+      )}
 
     </main>
   );
