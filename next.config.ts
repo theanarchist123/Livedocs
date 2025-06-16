db8b7f46765c8437dafcefdb6f2fa92353b297bb
theanarchist123 <2023.nikhil.kadam@ves.ac.in>
2025-06-16 21:37:32 +0530

    Typescript error fixed..

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript:{
+    // Enable type checking during build
+    ignoreBuildErrors: false,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'img.clerk.com' },
